7d04b20d00f483d08cb23c560c1ec575acfcc287
harveydb <harveydb@debianharv>
2022-06-27 19:46:38 +0200

    This code contains the conditional use

---
+++ conditions.py
@@ -0,0 +1,13 @@
+edad = int(input("Ingrese tu edad: "))
+if edad > 17:
+  print("Es mayor de edad")
+else:
+  print("Es menor de edad")
+
+numero = int(input("Ingrese un número: "))
+if numero > 5:
+  print("Es mayor a 5")
+elif numero == 5:
+  print("El número es igual a 5")
+else:
+  print("El menor a 5")
