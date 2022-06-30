7c9d329c9ef5116e4553c74d5e2ffd804affe926
harvey <193837@unsaac.edu.pe>
2022-06-30 21:44:32 +0200

    Added prueba_primalidad.py for to check a number is primo

--- bucles.py
+++ bucles.py
@@ -1,4 +1,5 @@
 import math
+from pydoc import doc
 
 def run():
   LIMETE = 1000
@@ -9,7 +10,7 @@ def run():
     print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
     contador = contador + 1
     potencia_2 = 2 ** contador
-
+    
 
 if __name__ == '__main__':
   run()
